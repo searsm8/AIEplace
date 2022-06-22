e79789df22fa3c1c636257438dfc194dcbeaf9e6
searsm8 <searsm8@gmail.com>
2022-06-22 15:06:56 -0700

    Add fft.cpp from RePlace

---
+++ math/fft/run_fft.cpp
@@ -0,0 +1,81 @@
+#include <cmath>
+#include <iostream>
+#include "fft.h"
+
+using namespace std;
+using namespace replace;
+
+
+int main() {
+    
+    cout << "FFT test..." << endl;
+    int M = 4;
+    int size = 16;
+    replace::FFT* fft = new replace::FFT(M, M, size, size);
+
+    for(int x = 0; x < M; x++)
+        for(int y = 0; y < M; y++)
+            fft->updateDensity(x, y, 1*x+3*y);
+    
+    for(int x = 0; x < M; x++) {
+        for(int y = 0; y < M; y++) {
+            cout << fft->getDensity(x, y) << " ";
+        }
+        cout << endl;
+    }
+
+    fft->doFFT();
+    
+    cout << "\nDensity:" << endl;
+    for(int x = 0; x < M; x++) {
+        for(int y = 0; y < M; y++) {
+            cout << fft->getDensity(x, y) << " ";
+        }
+        cout << endl;
+    }
+
+    cout << "\nelectro phi:" << endl;
+    for(int x = 0; x < M; x++) {
+        for(int y = 0; y < M; y++) {
+            cout << fft->getElectroPhi(x, y) << " ";
+        }
+        cout << endl;
+    }
+    cout << "\nelectro force X:" << endl;
+    for(int x = 0; x < M; x++) {
+        for(int y = 0; y < M; y++) {
+            cout << fft->getElectroForce(x, y).first << " ";
+        }
+        cout << endl;
+    }
+
+
+}
+/*
+    int M = 4;
+    float** binDensity_ = new float*[M];
+    std::vector<int> workArea_;
+    workArea_.resize( round(sqrt(std::max(M, M))) + 2, 0 );
+
+    std::vector<float> csTable_;
+    csTable_.resize( std::max(M, M) * 3 / 2, 0 );
+
+    for(int i = 0; i < M; i++) {
+        binDensity_[i] = new float[M];
+        for(int j = 0; j < M; j++){
+            binDensity_[i][j] = 1.0f;
+        }
+    }
+    for(int i = 0; i < M; i++) {
+        cout << "[";
+        for(int j = 0; j < M; j++) {
+            cout << binDensity_[i][j] << " ";
+        }
+        cout << "]" << endl;
+    }
+
+    replace::ddct2d(M, M, -1, binDensity_, // -1 means DCT
+      NULL, (int*) &workArea_[0], (float*)&csTable_[0]);
+
+}
+*/
