2232f834e3af7688309d4a13ac17d3d9f1fe70b3
searsm8 <searsm8@gmail.com>
2024-02-18 00:53:28 +0000

    Add back host code to Vitis project

---
+++ Vitis/host/src/include/Common.h
@@ -0,0 +1,47 @@
+#ifndef COMMON_H
+#define COMMON_H
+
+#include <iostream>
+#include <iomanip>
+#include <vector>
+#include <map>
+#include <algorithm>
+#include <cassert>
+#include <math.h>
+
+#include <experimental/filesystem>
+namespace fs = std::experimental::filesystem;
+
+using std::cout;
+using std::endl;
+using std::string;
+using std::vector;
+using std::map;
+using std::max;
+using std::min;
+
+#define AIEPLACE_NAMESPACE_BEGIN namespace AIEplace {
+#define AIEPLACE_NAMESPACE_END }
+
+#define WIRELENGTH_COMPUTE_METHOD "HPWL"
+//#define USE_AIE_ACCELERATION
+//#define CREATE_VISUALIZATION
+
+#define BINS_PER_ROW 128
+#define BINS_PER_COL 128
+#define INITIAL_LAMBDA 0.002
+
+AIEPLACE_NAMESPACE_BEGIN 
+
+typedef float position_type;
+
+struct XY
+{
+    double x;
+    double y;
+    void clear() { x = 0; y = 0;}
+};
+
+AIEPLACE_NAMESPACE_END 
+
+#endif
