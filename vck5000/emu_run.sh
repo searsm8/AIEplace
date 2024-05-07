6daedef9f616a50741e4dbed9e2b660ca3b64d10
searsm8 <searsm8@gmail.com>
2024-05-07 23:42:35 +0100

    Remove application run scripts, but added  Make commands to replace them.

--- vck5000/emu_run.sh
+++
@@ -1,3 +0,0 @@
-export XCL_EMULATION_MODE=hw_emu
-clear
-bin/AIEplace.exe host/benchmarks/ispd2015/mgc_fft_2/ bin/aieplace.hw_emu.xclbin
--- vck5000/hw_run.sh
+++
@@ -1,3 +0,0 @@
-unset XCL_EMULATION_MODE
-clear
-bin/AIEplace.exe host/benchmarks/ispd2015/mgc_fft_2/ bin/aieplace.hw.xclbin
