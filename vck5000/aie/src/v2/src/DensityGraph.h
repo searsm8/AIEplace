32411a48a8eb3e740d2114a7d01d745f66af72cf
Geert Roks <31696350+GeertRoks@users.noreply.github.com>
2026-07-03 17:24:18 +0200

    WIP(density): add fft and ifft graphs

--- vck5000/aie/src/v2/src/DensityGraph.h
+++ vck5000/aie/src/v2/src/DensityGraph.h
@@ -8,6 +8,7 @@
 #include "fft_ifft_dit_1ch_graph.hpp"
 namespace dsplib = xf::dsp::aie;
 
+#define DENSITY_CHANNELS 8
 #define FFT_POINTSIZE 1024
 #define FFT_CASCADE_LEN 10
 #define FFT_BITSHIFT 0
@@ -18,8 +19,7 @@ class FFTGraph : public adf::graph {
     adf::port<input> in;
     adf::port<output> out;
 
-    FFTGraph()
-    {
+    FFTGraph() {
       dsplib::fft::dit_1ch::fft_ifft_dit_1ch_graph<
         cfloat,                // data type
         cfloat,                // twiddle type
@@ -30,32 +30,92 @@ class FFTGraph : public adf::graph {
         FFT_DYNAMIC_POINTSIZE       // 0=disable, 1=enable dynamic pointsize
         > fft_pipeline;
 
-      runtime<ratio>(*fft_pipeline.getKernels()) = 1.0;
+      adf::kernel* fft_kernels = fft_pipeline.getKernels();
+      for (int i = 0; i < FFT_CASCADE_LEN; i++) {
+      	runtime<ratio>(*(fft_kernels+i)) = 1.0;
+      }
 
       adf::connect<adf::stream>(in, fft_pipeline.in[0]);
       adf::connect<adf::stream>(fft_pipeline.out[0], out);
 
     }
+
+    void place(int start_col, int row) {
+      adf::location<adf::graph>(*this) = area_group({
+        {aie_tile,  start_col,                   row,
+                    start_col + (FFT_CASCADE_LEN/2), row+1 },
+        {shim_tile, 0, 0, 49, 0}
+      });
+    }
+};
+
+class IFFTGraph : public adf::graph {
+  public:
+    adf::port<input> in;
+    adf::port<output> out;
+
+    IFFTGraph() {
+      dsplib::fft::dit_1ch::fft_ifft_dit_1ch_graph<
+        cfloat,                // data type
+        cfloat,                // twiddle type
+        FFT_POINTSIZE,              // length of a row or col (should be num of rows/cols, assuming square matrix)
+        0,                          // 1=FFT, 0=iFFT
+        FFT_BITSHIFT,               // bitshift before output
+        FFT_CASCADE_LEN,            // Pipeline cascade length (max log2(FFT_POINTSIZE)
+        FFT_DYNAMIC_POINTSIZE       // 0=disable, 1=enable dynamic pointsize
+        > ifft_pipeline;
+
+      adf::kernel* fft_kernels = ifft_pipeline.getKernels();
+      for (int i = 0; i < FFT_CASCADE_LEN; i++) {
+      	runtime<ratio>(*(fft_kernels+i)) = 1.0;
+      }
+
+      adf::connect<adf::stream>(in, ifft_pipeline.in[0]);
+      adf::connect<adf::stream>(ifft_pipeline.out[0], out);
+
+    }
+
+    void place(int start_col, int row) {
+      adf::location<adf::graph>(*this) = area_group({
+        {aie_tile,  start_col,                   row,
+                    start_col + (FFT_CASCADE_LEN/2), row+1 },
+        {shim_tile, 0, 0, 49, 0}
+      });
+    }
 };
 
 class DensityGraph : public adf::graph {
   public:
-    adf::input_plio fft_in;
-    adf::output_plio fft_out;
+    adf::input_plio fft_in[DENSITY_CHANNELS];
+    adf::output_plio fft_out[DENSITY_CHANNELS];
+    adf::input_plio ifft_in[DENSITY_CHANNELS];
+    adf::output_plio ifft_out[DENSITY_CHANNELS];
 
-    FFTGraph fft;
+    FFTGraph fft[DENSITY_CHANNELS];
+    IFFTGraph ifft[DENSITY_CHANNELS];
 
     DensityGraph() {
-      adf::location<FFTGraph>(fft) = area_group({
-          {aie_tile, 0, 0, 10, 0},
-          {shim_tile, 0, 0, 49, 0}
-          });
 
-      fft_in = adf::input_plio::create("fft_in", adf::plio_128_bits, "test_data/density/input.dat");
-      fft_out = adf::output_plio::create("fft_out", adf::plio_128_bits, "test_data/density/golden.dat");
+      for (int i = 0; i < DENSITY_CHANNELS; i++) {
 
-      adf::connect<adf::stream>(fft_in.out[0], fft.in);
-      adf::connect<adf::stream>(fft.out, fft_out.in[0]);
+	fft[i].place((i%2)*((FFT_CASCADE_LEN/2)+1), (i & ~1));
+
+        fft_in[i] = adf::input_plio::create("fft_in"+std::to_string(i), adf::plio_128_bits, "test_data/density/input.dat");
+        fft_out[i] = adf::output_plio::create("fft_out"+std::to_string(i), adf::plio_128_bits, "test_data/density/golden.dat");
+
+        adf::connect<adf::stream>(fft_in[i].out[0], fft[i].in);
+        adf::connect<adf::stream>(fft[i].out, fft_out[i].in[0]);
+
+	ifft[i].place((i%2)*((FFT_CASCADE_LEN/2)+1)+12, (i & ~1));
+
+        ifft_in[i] = adf::input_plio::create("ifft_in"+std::to_string(i), adf::plio_128_bits, "test_data/density/input.dat");
+        ifft_out[i] = adf::output_plio::create("ifft_out"+std::to_string(i), adf::plio_128_bits, "test_data/density/golden.dat");
+
+        adf::connect<adf::stream>(ifft_in[i].out[0], ifft[i].in);
+        adf::connect<adf::stream>(ifft[i].out, ifft_out[i].in[0]);
+
+      }
     }
 
 };
+
--- vck5000/aie/src/v2/src/TopGraph.h
+++ vck5000/aie/src/v2/src/TopGraph.h
@@ -3,8 +3,6 @@
 #include <adf.h>
 #include "DensityGraph.h"
 
-#define DENSITY_CHANNELS 1
-
 class AIEplaceGraph : public adf::graph {
   public:
     DensityGraph density;
